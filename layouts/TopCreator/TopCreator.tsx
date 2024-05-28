dbfebffb46d3fdac49460f5f10d2943f217231e9
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-05-28 04:31:04 +0330

    Add AOS for Top Creator🎄

--- layouts/TopCreator/TopCreator.tsx
+++ layouts/TopCreator/TopCreator.tsx
@@ -11,22 +11,22 @@ const TopCreator = () => {
         </h1>
       </div>
       <div className="w-[80%] mx-auto pt-[5rem] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-[1rem]">
-        <div className="">
+        <div data-aos='zoom-in' data-aos-achor-placement="top-center">
           <CreatorCard image={"AvatarOne"} user={"Douglas Williams"} price={"734.5"} />
         </div>
-        <div className="">
+        <div data-aos='zoom-in' data-aos-achor-placement="top-center" data-aos-delay="100">
           <CreatorCard image={"AvatarTwo"} user={"James White"} price={"804.5"} />
         </div>
-        <div className="">
+        <div data-aos='zoom-in' data-aos-achor-placement="top-center" data-aos-delay="200">
           <CreatorCard image={"AvatarThree"} user={"Clarence Wheeler"} price={"624.5"} />
         </div>
-        <div className="">
+        <div data-aos='zoom-in' data-aos-achor-placement="top-center" data-aos-delay="300">
           <CreatorCard image={"AvatarFour"} user={"Beverly Perry"} price={"948.5"} />
         </div>
-        <div className="">
+        <div data-aos='zoom-in' data-aos-achor-placement="top-center" data-aos-delay="400">
           <CreatorCard image={"AvatarFive"} user={"John Kennedy"} price={"325.5"} />
         </div>
-        <div className="">
+        <div data-aos='zoom-in' data-aos-achor-placement="top-center" data-aos-delay="500">
           <CreatorCard image={"AvatarSix"} user={"Sarah Perry"} price={"764.5"} />
         </div>
       </div>
